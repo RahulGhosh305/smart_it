923378241e47627868c5e3cab3875a1ae0e96639
Rahul Ghosh <rahulghosh096@gmail.com>
2020-10-21 20:37:10 +0600

    version 1 topbar navbar slider servicePart workPart

--- Js/custom.js
+++ Js/custom.js
@@ -21,3 +21,50 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+//* Navbar click with hide ########################################################
+$("document").ready(function () {
+    //* selector on event
+    $(".jquery-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+});
+
+
+
+//* Owl-carousel Slider Code ########################################################
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        autoplayTimeout: 6000,
+        loop:true,
+        margin:10,
+        responsive:{
+            0:{
+                items:1
+             }
+        }
+    }) 
+});
+
+
+// isotope build plugin code start
+
+//* document ready
+$("document").ready(function () {
+    //* init Isotope
+    var $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+});
+// isotope code end
+
